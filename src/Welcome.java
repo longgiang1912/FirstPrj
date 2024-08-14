1e42c38c6111e524c2f08e9b6a53a09f3d48f59a
longgiang1912 <23694641.giang@student.iuh.edu.vn>
2024-08-14 12:40:04 +0700

    First commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Hello Ho Long Giang!");
+        System.out.println("Welcome to the DHKTPM19B!");
+    }
+}
